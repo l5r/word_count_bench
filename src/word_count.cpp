54b92f083a4fbc14888d179925a125c904856a7d
Leander Lismond <36822732+l5r@users.noreply.github.com>
2024-06-02 21:32:24 +0200

    Add total timers to word_count_cpp

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -59,9 +59,35 @@ std::multimap<int, std::string, std::greater<int> > word_count(std::basic_istrea
 }
 
 int main() {
+  using namespace std::chrono;
+
+  auto count_start_time_mono = steady_clock::now();
+  auto count_start_time_wall = system_clock::now();
+
   auto counted_words = word_count(std::cin);
+
+  auto count_duration_mono = duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
+  auto count_duration_wall = duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
+
+  std::cerr << "count time (mono): " << count_duration_mono.count() << "ms" << std::endl;
+  std::cerr << "count time (wall): " << count_duration_wall.count() << "ms" << std::endl;
+
+  auto print_start_time_mono = steady_clock::now();
+  auto print_start_time_wall = system_clock::now();
 
   for (std::pair<int, std::string>counted_word: counted_words) {
     std::cout << counted_word.first << "\t" << counted_word.second << std::endl;
   }
+
+  auto print_duration_mono = duration_cast<milliseconds>(steady_clock::now() - print_start_time_mono);
+  auto print_duration_wall = duration_cast<milliseconds>(system_clock::now() - print_start_time_wall);
+
+  std::cerr << "print time (mono): " << print_duration_mono.count() << "ms" << std::endl;
+  std::cerr << "print time (wall): " << print_duration_wall.count() << "ms" << std::endl;
+
+  auto total_duration_mono = duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
+  auto total_duration_wall = duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
+
+  std::cerr << "total time (mono): " << total_duration_mono.count() << "ms" << std::endl;
+  std::cerr << "total time (wall): " << total_duration_wall.count() << "ms" << std::endl;
 }
