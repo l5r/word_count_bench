c7486ffed99a488dd13c77029fd17773d76bcd7e
Leander Lismond <36822732+l5r@users.noreply.github.com>
2024-06-02 19:33:43 +0200

    Add timers to word_count_cpp

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,3 +1,4 @@
+#include <bits/chrono.h>
 #include <cctype>
 #include <functional>
 #include <iostream>
@@ -11,9 +12,15 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
+#include <chrono>
 
 std::multimap<int, std::string, std::greater<int> > word_count(std::basic_istream<char> &text) {
+  using namespace std::chrono;
+
   std::unordered_map<std::string, int> word_counts;
+
+  auto read_start_time_mono = steady_clock::now();
+  auto read_start_time_wall = system_clock::now();
 
   std::string word;
   char c;
@@ -28,10 +35,25 @@ std::multimap<int, std::string, std::greater<int> > word_count(std::basic_istrea
     }
   }
 
+  auto read_duration_mono = duration_cast<milliseconds>(steady_clock::now() - read_start_time_mono);
+  auto read_duration_wall = duration_cast<milliseconds>(system_clock::now() - read_start_time_wall);
+
+  std::cerr << "read time (mono): " << read_duration_mono.count() << "ms" << std::endl;
+  std::cerr << "read time (wall): " << read_duration_wall.count() << "ms" << std::endl;
+
+  auto sort_start_time_mono = steady_clock::now();
+  auto sort_start_time_wall = system_clock::now();
+
   std::multimap<int, std::string, std::greater<int> > counted_words;
   for (std::pair<std::string, int> word_count: word_counts) {
     counted_words.insert(std::pair<int, std::string>(word_count.second, word_count.first));
   }
+
+  auto sort_duration_mono = duration_cast<milliseconds>(steady_clock::now() - sort_start_time_mono);
+  auto sort_duration_wall = duration_cast<milliseconds>(system_clock::now() - sort_start_time_wall);
+
+  std::cerr << "sort time (mono): " << sort_duration_mono.count() << "ms" << std::endl;
+  std::cerr << "sort time (wall): " << sort_duration_wall.count() << "ms" << std::endl;
 
   return counted_words;
 }
@@ -40,10 +62,6 @@ int main() {
   auto counted_words = word_count(std::cin);
 
   for (std::pair<int, std::string>counted_word: counted_words) {
-    if (counted_word.second.length() < 7) {
-    std::cout << counted_word.second << ":\t\t" << counted_word.first << std::endl;
-      } else {
-    std::cout << counted_word.second << ":\t" << counted_word.first << std::endl;
-    }
+    std::cout << counted_word.first << "\t" << counted_word.second << std::endl;
   }
 }
