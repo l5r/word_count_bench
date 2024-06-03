5bdef4a5b15951541bdb76c96940de573c3f1f66
Leander Lismond <36822732+l5r@users.noreply.github.com>
2024-06-03 04:05:33 +0200

    Add better input handling to word_count_cpp
    Allow users to specify input and output on the command line

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,5 +1,7 @@
 #include <bits/chrono.h>
 #include <cctype>
+#include <chrono>
+#include <fstream>
 #include <functional>
 #include <iostream>
 #include <istream>
@@ -12,9 +14,9 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <chrono>
 
-std::multimap<int, std::string, std::greater<int> > word_count(std::basic_istream<char> &text) {
+std::multimap<int, std::string, std::greater<int>>
+word_count(std::basic_istream<char> &text) {
   using namespace std::chrono;
 
   std::unordered_map<std::string, int> word_counts;
@@ -35,59 +37,124 @@ std::multimap<int, std::string, std::greater<int> > word_count(std::basic_istrea
     }
   }
 
-  auto read_duration_mono = duration_cast<milliseconds>(steady_clock::now() - read_start_time_mono);
-  auto read_duration_wall = duration_cast<milliseconds>(system_clock::now() - read_start_time_wall);
+  auto read_duration_mono =
+      duration_cast<milliseconds>(steady_clock::now() - read_start_time_mono);
+  auto read_duration_wall =
+      duration_cast<milliseconds>(system_clock::now() - read_start_time_wall);
 
-  std::cerr << "read time (mono): " << read_duration_mono.count() << "ms" << std::endl;
-  std::cerr << "read time (wall): " << read_duration_wall.count() << "ms" << std::endl;
+  std::cerr << "read time (mono): " << read_duration_mono.count() << "ms"
+            << std::endl;
+  std::cerr << "read time (wall): " << read_duration_wall.count() << "ms"
+            << std::endl;
 
   auto sort_start_time_mono = steady_clock::now();
   auto sort_start_time_wall = system_clock::now();
 
-  std::multimap<int, std::string, std::greater<int> > counted_words;
-  for (std::pair<std::string, int> word_count: word_counts) {
-    counted_words.insert(std::pair<int, std::string>(word_count.second, word_count.first));
+  std::multimap<int, std::string, std::greater<int>> counted_words;
+  for (std::pair<std::string, int> word_count : word_counts) {
+    counted_words.insert(
+        std::pair<int, std::string>(word_count.second, word_count.first));
   }
 
-  auto sort_duration_mono = duration_cast<milliseconds>(steady_clock::now() - sort_start_time_mono);
-  auto sort_duration_wall = duration_cast<milliseconds>(system_clock::now() - sort_start_time_wall);
+  auto sort_duration_mono =
+      duration_cast<milliseconds>(steady_clock::now() - sort_start_time_mono);
+  auto sort_duration_wall =
+      duration_cast<milliseconds>(system_clock::now() - sort_start_time_wall);
 
-  std::cerr << "sort time (mono): " << sort_duration_mono.count() << "ms" << std::endl;
-  std::cerr << "sort time (wall): " << sort_duration_wall.count() << "ms" << std::endl;
+  std::cerr << "sort time (mono): " << sort_duration_mono.count() << "ms"
+            << std::endl;
+  std::cerr << "sort time (wall): " << sort_duration_wall.count() << "ms"
+            << std::endl;
 
   return counted_words;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   using namespace std::chrono;
+
+  std::istream *input = &std::cin;
+  std::ostream *output = &std::cout;
+
+  std::ifstream input_file;
+  std::ofstream output_file;
+
+  char *input_filename = NULL, *output_filename = NULL;
+
+  switch (argc) {
+  case 1:
+    break;
+  case 2:
+    input_filename = argv[1];
+    break;
+  case 3:
+    input_filename = argv[1];
+    output_filename = argv[2];
+    break;
+  default:
+    std::cerr << "Wrong number of arguments, expected up to two arguments but got:" << (argc - 1) << std::endl;
+    break;
+  }
+
+  if (input_filename) {
+    input_file.open(input_filename);
+    if (input_file.is_open()) {
+      input = &input_file;
+    } else {
+      std::cerr << "Failed to open input file: " << input_filename << std::endl;
+      return 1;
+    }
+  }
+
+  if (output_filename) {
+    output_file.open(output_filename);
+    if (output_file.is_open()) {
+      output = &output_file;
+    } else {
+      std::cerr << "Failed to open output file: " << output_filename << std::endl;
+      return 1;
+    }
+    
+  }
 
   auto count_start_time_mono = steady_clock::now();
   auto count_start_time_wall = system_clock::now();
 
-  auto counted_words = word_count(std::cin);
+  auto counted_words = word_count(*input);
 
-  auto count_duration_mono = duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
-  auto count_duration_wall = duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
+  auto count_duration_mono =
+      duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
+  auto count_duration_wall =
+      duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
 
-  std::cerr << "count time (mono): " << count_duration_mono.count() << "ms" << std::endl;
-  std::cerr << "count time (wall): " << count_duration_wall.count() << "ms" << std::endl;
+  std::cerr << "count time (mono): " << count_duration_mono.count() << "ms"
+            << std::endl;
+  std::cerr << "count time (wall): " << count_duration_wall.count() << "ms"
+            << std::endl;
 
   auto print_start_time_mono = steady_clock::now();
   auto print_start_time_wall = system_clock::now();
 
-  for (std::pair<int, std::string>counted_word: counted_words) {
-    std::cout << counted_word.first << "\t" << counted_word.second << std::endl;
+  for (std::pair<int, std::string> counted_word : counted_words) {
+    *output << counted_word.first << "\t" << counted_word.second << std::endl;
   }
 
-  auto print_duration_mono = duration_cast<milliseconds>(steady_clock::now() - print_start_time_mono);
-  auto print_duration_wall = duration_cast<milliseconds>(system_clock::now() - print_start_time_wall);
+  auto print_duration_mono =
+      duration_cast<milliseconds>(steady_clock::now() - print_start_time_mono);
+  auto print_duration_wall =
+      duration_cast<milliseconds>(system_clock::now() - print_start_time_wall);
 
-  std::cerr << "print time (mono): " << print_duration_mono.count() << "ms" << std::endl;
-  std::cerr << "print time (wall): " << print_duration_wall.count() << "ms" << std::endl;
+  std::cerr << "print time (mono): " << print_duration_mono.count() << "ms"
+            << std::endl;
+  std::cerr << "print time (wall): " << print_duration_wall.count() << "ms"
+            << std::endl;
 
-  auto total_duration_mono = duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
-  auto total_duration_wall = duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
+  auto total_duration_mono =
+      duration_cast<milliseconds>(steady_clock::now() - count_start_time_mono);
+  auto total_duration_wall =
+      duration_cast<milliseconds>(system_clock::now() - count_start_time_wall);
 
-  std::cerr << "total time (mono): " << total_duration_mono.count() << "ms" << std::endl;
-  std::cerr << "total time (wall): " << total_duration_wall.count() << "ms" << std::endl;
+  std::cerr << "total time (mono): " << total_duration_mono.count() << "ms"
+            << std::endl;
+  std::cerr << "total time (wall): " << total_duration_wall.count() << "ms"
+            << std::endl;
 }
