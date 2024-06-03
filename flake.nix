d4b233936aff67990a3ccdfa619815efd45cff05
Leander Lismond <36822732+l5r@users.noreply.github.com>
2024-06-03 04:07:07 +0200

    Add word_count_cpp to flake

--- flake.nix
+++ flake.nix
@@ -16,13 +16,25 @@
         packages = rec {
           benchkit-python = benchkit.legacyPackages.${system}.benchkit-python;
           benchkit-python-dev = pkgs.python3.withPackages (ps: [
-            benchkit.legacyPackages.${system}.benchkit-python
+            benchkit.legacyPackages.${system}.benchkit
             ps.python-lsp-server
             ps.python-lsp-server.optional-dependencies.rope
             ps.python-lsp-server.optional-dependencies.pyflakes
           ]);
 
           word-count = word-count-src.legacyPackages.${system}.default;
+          word-count-cpp = pkgs.stdenv.mkDerivation {
+            name = "word-count-cpp";
+            src = ./src;
+
+            buildPhase = ''
+              c++ -O2 word_count.cpp -o word_count
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp word_count $out/bin
+            '';
+          };
         };
       });
 }
